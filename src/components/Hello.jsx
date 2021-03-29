0c81619ba3209885ad7bf68ef85fcf640de29bd7
hkarube <karubehiroyuki08@yahoo.co.jp>
2021-03-29 15:23:31 +0900

    add styles

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 function Hello() {
   return (
     <View>
-      <Text>Hello</Text>
+      <Text style={styles.Text}>Hello</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  Text: {
+    color: '#ffffff',
+    backgroundColor: 'blue',
+    fontSize: 40,
+    fontWeight: 'bolt',
+    padding: 16,
+  },
+});
 
 export default Hello;
