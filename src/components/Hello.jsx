b944c08f7112b13b7fbc770a3b8d6a418e823c68
hkarube <karubehiroyuki08@yahoo.co.jp>
2021-03-31 15:39:50 +0900

    update 0331

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { string } from 'prop-types';
 
-function Hello() {
+function Hello(props) {
+  const { children } = props;
   return (
     <View>
-      <Text style={styles.Text}>Hello</Text>
+      <Text tyle={styles.Text}>
+          hello {children}
+        </Text>
     </View>
   );
 }
+
+Hello.propTypes = {
+  children: string.isReqired,
+};
 
 const styles = StyleSheet.create({
   Text: {
