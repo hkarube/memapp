9404f403e6f3bf73d9d58e065ce2ca499a928743
hkarube <karubehiroyuki08@yahoo.co.jp>
2021-05-18 14:32:28 +0900

    test

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+
 import AppBar from '../components/AppBar';
 import CircleBotton from '../components/CircleButton';
 
 export default function MemoDetailScreen() {
   return (
-    <View>
-      <AppBar>
-        <View>
-          <Text>買い物リスト</Text>
-          <Text>2020/23/24 10:00</Text>
-        </View>
-        <View>
-          <Text>
-            買い物詳細
-          </Text>
-        </View>
-        <CircleBotton>+</CircleBotton>
-      </AppBar>
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.memHeader}>
+        <Text style={styles.memoListTitl}>買い物リスト</Text>
+        <Text style={styles.memoListItemDate}>2020/23/24 10:00</Text>
+      </View>
+      <ScrollView style={styles.memoBoby}>
+        <Text tyle={styles.memoText}>
+          買い物詳細
+        </Text>
+      </ScrollView>
+      <CircleBotton style={{ top: 160, bottom: 'auto' }}>+</CircleBotton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  memHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoListTitl: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoListItemDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoBoby: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
